ea022a478b98f15e18728d6b9d7b52067fcecbf4
Thunay Moreira de Soares <thunay@buildbox.com.br>
2026-01-22 23:39:11 -0300

    🎉

---
+++ book-finder/resources/js/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Head } from '@inertiajs/react';
+
+export default function Home() {
+    return (
+        <>
+            <Head title="Book Finder - Página Inicial"></Head>
+
+            <main>aaaaaaa</main>
+        </>
+    );
+}
